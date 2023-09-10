4310064cfa7a0ad23aac69e0749b81842b816c85
thmsbawa <newbgng01@gmail.com>
2023-09-10 13:20:02 +0000

    Programming in c for the first time

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * Entry point - Prints the size of various data types on computer
- *
- * Return: Always 0 (Success)
+ * Entry point - This functions prints the size of data types on computer
+ * Return:Always 0 (Success)
  */
 int main(void)
 {
@@ -13,10 +12,10 @@ int main(void)
 	long int int2;
 	long long int int3;
 
-	printf("Size of a char:%lu\n", (unsigned long)sizeof(char1));
-	printf("Size of an int:%lu\n", (unsigned long)sizeof(int1));
-	printf("Size of a long int:%lu\n", (unsigned long)sizeof(int2));
-	printf("Size of a long long int:%lu\n", (unsigned long)sizeof(int3));
-	printf("Size of a float:%lu\n", (unsigned long)sizeof(flt));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char1));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int1));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(int2));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(int3));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(flt));
 	return (0);
 }
---
+++ 0x00-hello_world/see.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * Entry point - Prints the size of various data types on computer
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int int1;
+        char char1;
+        float flt;
+        long int int2;
+        long long int int3;
+
+        printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char1));
+        printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int1));
+        printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(int2));
+        printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(int3));
+        printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(flt));
+        return (0);
+
