b5d06114fadebbfdff88c2417dc558e7e0e97a6f
thmsbawa <newbgng01@gmail.com>
2023-09-13 00:17:33 +0000

    Writing my first functions in c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,14 @@ void print_alphabet(void);
  */
 void print_alphabet_x10(void)
 {
-	int i;
+	int i, j;
 
 	i = 0;
 	while (i < 10)
 	{
-		print_alphabet();
+		for(j = 'a'; j <= 'z'; j++)	
+		_putchar(j);
+		_putchar('\n');
 		i++;
 	}
 }
