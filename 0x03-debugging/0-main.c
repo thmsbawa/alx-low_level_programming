04d78ec354d0ace3f8447ae3b6acfc9ed4ac1718
thmsbawa <newbgng01@gmail.com>
2023-09-14 14:43:56 +0000

    Learning to debug

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if int if negative or positive
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/test.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+int main(void)
+{
+	int n, a, b, c;
+	a = 5;
+	b = 8;
+	c = 3;
+
+	n = largest_number(a, b, c);
+	printf("%d\n", n);
+	return (0);
+}
