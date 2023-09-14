19a920d8da6eacf80dc957cbf05f02209d0297a1
thmsbawa <newbgng01@gmail.com>
2023-09-14 19:06:35 +0000

    Learning to debug

---
+++ 0x03-debugging/3-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int month, day, year;
+
+	month = 4;
+	day = 01;
+	year = 1997;
+
+	printf("Date: %02d/%02d/%04d\n", month, day, year);
+
+	day = convert_day(month, day);
+
+	print_remaining_days(month, day, year);
+
+	return (0);
+}
