9e9d29c0ef705bce3c69c7ca9440399cbd8d22eb
thmsbawa <newbgng01@gmail.com>
2023-09-15 21:11:02 +0000

    Writing my first c fumctions

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,11 +11,11 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3) == 0)
+		if ((i % 3) == 0 && (i % 15) != 0)
 		{
 			printf("Fizz");
 		}
-		else if ((i % 5) == 0)
+		else if ((i % 5) == 0 && (i % 15) != 0)
 		{
 			printf("Buzz");
 		}
