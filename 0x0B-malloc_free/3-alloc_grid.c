86d6446b9a7d1435a73561cc32f957c58a44a28c
thmsbawa <newbgng01@gmail.com>
2023-10-04 19:15:50 +0000

    memory allocation

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	ptr = (int **)malloc(height * sizeof(p));
+	ptr = (int **)malloc(height * sizeof(ptr));
 	if (ptr == NULL)
 	{
 		free(ptr);
