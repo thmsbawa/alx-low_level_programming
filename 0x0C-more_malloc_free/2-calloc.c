e965f578758fc3863c2f1a767c7f533c68d7cab8
thmsbawa <newbgng01@gmail.com>
2023-10-19 17:19:17 +0000

    catching up malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -19,9 +19,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < nmemb; i++)
-	{
-		ptr[i] = 0;
-	}
+	memset(ptr, 0, (nmemb * size));
 	return (ptr);
 }
